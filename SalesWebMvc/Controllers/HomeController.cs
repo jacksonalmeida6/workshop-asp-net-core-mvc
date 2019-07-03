4c247bdd5e36adc99ce5758f607b848960e47ec7
Jackson Almeida de Oliveira <jacksonalmeida6@gmail.com>
2019-07-03 15:39:01 -0300

    MySql adaptation and first migration

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models.ViewModels;
 
@@ -33,6 +29,7 @@ namespace SalesWebMvc.Controllers
         {
             return View();
         }
+        public IActionResult Departaments => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
